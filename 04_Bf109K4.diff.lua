b5c0c69c992513bd16add9359f0247d69a4d3817
tek_604 <tek.604@gmail.com>
2024-07-21 09:15:16 +0200

    First version

---
+++ 04_Bf109K4.diff.lua
@@ -0,0 +1,101 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3014pnilunilcd3vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN24",
+				},
+			},
+			["name"] = "Bomb Fusing Selector (next mode)",
+		},
+		["d3016pnilunilcd3vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN23",
+				},
+			},
+			["name"] = "Bomb Fusing Selector (prev. mode)",
+		},
+		["d3025pnilunilcd1vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN30",
+				},
+			},
+			["name"] = "CB V101 - Wing / Drop Ordnance (toggle)",
+		},
+		["d3032pnilunilcd1vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN28",
+				},
+			},
+			["name"] = "SZKK 3 Weapon Control OFF",
+		},
+		["d3032pnilunilcd1vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN26",
+				},
+			},
+			["name"] = "SZKK 3 Weapon Control ON",
+		},
+		["d3034pnilunilcd1vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "Wing Cannons OFF",
+		},
+		["d3034pnilunilcd1vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "Wing Cannons ON",
+		},
+		["d3053pnilu3053cd1vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN31",
+				},
+			},
+			["name"] = "Bomb Release (B2)",
+		},
+		["d3055pnilu3055cd1vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+			["name"] = "Charge MK 108 (D)",
+		},
+		["d3074pnilunilcd1vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "Guns Safety Lever (toggle)",
+		},
+		["d350pnilu351cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "Fire Cannon / Rockets (B1)",
+		},
+		["d84pnilu85cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+			["name"] = "Fire Machineguns (A)",
+		},
+	},
+}
+return diff
